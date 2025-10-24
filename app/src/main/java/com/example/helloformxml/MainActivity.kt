f96ef4d0ee3310c31035d570a5a4031008a93245
Adán Romero <adanromeromarrero@gmail.com>
2025-10-24 11:28:28 +0200

    Mejora opcional de mostrar los caracteres que se escribe, y de boyón que no deja apretar si no se ha escrito el nombre. Hecho.

--- app/src/main/java/com/example/helloformxml/MainActivity.kt
+++ app/src/main/java/com/example/helloformxml/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.helloformxml
 
 import android.os.Bundle
+import android.text.Editable
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Button
@@ -16,6 +17,50 @@ class MainActivity : ComponentActivity() {
         val etNombre = findViewById<EditText>(R.id.etNombre)
         val btnSaludar = findViewById<Button>(R.id.btnSaludar)
         val tvMensaje = findViewById<TextView>(R.id.tvMensaje)
+        val caracteresContador = findViewById<TextView>(R.id.caracteresContador)
+
+
+        //Lógica para desactivar el botón de saludar si no se ha incertado el nombre
+        //Primero hago que el boton de saludar no esté disponible
+        btnSaludar.isEnabled = false
+
+        //Ahora hago uso de TextWatcher dentro de la variable "etNombre", para dentro de la lógica, elegir si el botón está disponible o no
+        etNombre.addTextChangedListener(object : android.text.TextWatcher{
+
+            //Este método lo implemeto porque sino "object" me da error
+            //De todas formas, tiene contenido que se ejecuta antes de cambiar el texto
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+
+            }
+
+            //Este método lo implemeto porque sino "object" me da error
+            //De todas formas, tiene contenido que se ejecuta mientras de cambiar el texto
+            override fun onTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+
+            }
+
+            //Coloco un método que mira si el texto tiene texto o no justo tras ser medificado
+            override fun afterTextChanged(esEditable : android.text.Editable?) {
+                //Si el contenido del texto no es nulo o está vacío, el botón se activará
+                btnSaludar.isEnabled = !esEditable.isNullOrEmpty()
+
+                //Añado lo referido a carcateres, su cantidad, y actualizar el texto
+                val cantidadCarcateres = esEditable?.length ?: 0
+                caracteresContador.text = "$cantidadCarcateres caracteres."
+            }
+
+        })
+
 
         //Hago la lógica del botón
         btnSaludar.setOnClickListener {
